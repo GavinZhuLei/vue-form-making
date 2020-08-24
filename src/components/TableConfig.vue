8696cf124e0929be7d975188a46eba2c92e1ef0b
upcwangying <upcwangying@126.com>
2020-08-24 15:25:27 +0800

    feat: 行式列表

--- src/components/TableConfig.vue
+++ src/components/TableConfig.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>TableConfig</div>
+  <div style="height: 100%; overflow: hidden">
+    <el-container style="height: 33.33% !important;">
+      <el-header height="40px">
+        <div class="config-tab all middle">列表属性</div>
+      </el-header>
+      <el-main>
+        列表属性
+      </el-main>
+    </el-container>
+    <el-container style="height: 33.33% !important;">
+      <el-header height="40px">
+        <div class="config-tab all middle">列表样式</div>
+      </el-header>
+      <el-main>
+        列表样式
+      </el-main>
+    </el-container>
+    <el-container style="height: 33.33% !important;">
+      <el-header height="40px">
+        <div class="config-tab all middle">单元格属性</div>
+      </el-header>
+      <el-main>
+        单元格属性
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
